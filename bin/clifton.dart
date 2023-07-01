9d2ad6fbd4d0272d1467c227ab909b252a86f04f
Bruno Goirand <geebe@goirand.com>
2023-07-02 00:03:36 +0200

    Added prompt

--- bin/clifton.dart
+++ bin/clifton.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
 void main() async {
+  //
+  // Get the prompt from environment variable if it is present
+  String? prompt = Platform.environment['cliftonPrompt'];
+  // Otherwise set the default value
+  prompt ??= '>';
+  //
   final client = await Socket.connect('localhost', 8765);
   print('Connected to server: ${client.remoteAddress}:${client.remotePort}');
 
+  // Display the prompt initially
+  stdout.write('$prompt ');
+
   client.listen((data) {
     final response = String.fromCharCodes(data).trim();
-    print('Response from server: $response');
+    print('\rResponse from server: $response');
+    // Display the prompt again
+    stdout.write('$prompt ');
   });
 
   stdin.listen((data) {
